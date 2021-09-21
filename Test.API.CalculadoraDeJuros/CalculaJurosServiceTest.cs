dbb65b708cf88165afd38671c559aaf80bd40a18
Thomaz Carvalho Leite Silveira <tcarvalholeitesilveira@gmail.com>
2021-09-21 12:13:44 -0300

    novos testes

--- Test.API.CalculadoraDeJuros/CalculaJurosServiceTest.cs
+++ Test.API.CalculadoraDeJuros/CalculaJurosServiceTest.cs
@@ -21,9 +21,47 @@ namespace Test.API.CalculadoraDeJuros
         {
             _taxaDeJurosMock.Setup(x => x.ObterTaxaDeJuros()).ReturnsAsync(0.01);
 
-            var resultado = await _sut.ValorFuturo(100, 5);
+            Assert.Equal(105.1, await _sut.ValorFuturo(100, 5));
+        }
 
-            Assert.Equal(105.1, resultado);
+        [Fact]
+        public async Task ValorFuturo_DeveraRetornarException_QuandoAlgumDosParametrosDeEntradaForNegativo()
+        {
+            _taxaDeJurosMock.Setup(x => x.ObterTaxaDeJuros()).ReturnsAsync(0.01);
+
+            ArgumentException exception = null;
+
+            exception = null;
+            exception = await Assert.ThrowsAsync<ArgumentException>(() => _sut.ValorFuturo(-1, 5));
+            Assert.Equal("Valor inserido não é válido, valorInicial necessita ser maior que zero (Parameter 'valorInicial')", exception.Message);
+
+            exception = null;
+            exception = await Assert.ThrowsAsync<ArgumentException>(() => _sut.ValorFuturo(100, -1));
+            Assert.Equal("Valor inserido não é válido, o número de meses necessita ser maior que zero (Parameter 'meses')", exception.Message);
+
+            exception = null;
+            exception = await Assert.ThrowsAsync<ArgumentException>(() => _sut.ValorFuturo(-1, -1));
+            Assert.Equal("Valor inserido não é válido, valorInicial necessita ser maior que zero (Parameter 'valorInicial')", exception.Message);
+        }
+
+        [Fact]
+        public async Task ValorFuturo_DeveraRetornarException_QuandoAlgumDosParametrosDeEntradaForZero()
+        {
+            _taxaDeJurosMock.Setup(x => x.ObterTaxaDeJuros()).ReturnsAsync(0.01);
+
+            ArgumentException exception = null;
+
+            exception = await Assert.ThrowsAsync<ArgumentException>(() => _sut.ValorFuturo(0, 5));
+            Assert.Equal("Valor inserido não é válido, valorInicial necessita ser maior que zero (Parameter 'valorInicial')", exception.Message);
+
+
+            exception = null;
+            exception = await Assert.ThrowsAsync<ArgumentException>(() => _sut.ValorFuturo(100, 0));
+            Assert.Equal("Valor inserido não é válido, o número de meses necessita ser maior que zero (Parameter 'meses')", exception.Message);
+
+            exception = null;
+            exception = await Assert.ThrowsAsync<ArgumentException>(() => _sut.ValorFuturo(0, 0));
+            Assert.Equal("Valor inserido não é válido, valorInicial necessita ser maior que zero (Parameter 'valorInicial')", exception.Message);
         }
     }
 }
---
+++ Test.API.CalculadoraDeJuros/TaxaDeJurosClientTest.cs
@@ -0,0 +1,38 @@
+﻿using API.CaluladoraDeJuros;
+using API.CaluladoraDeJuros.Clients;
+using Microsoft.Extensions.Options;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Test.API.CalculadoraDeJuros
+{
+    public class TaxaDeJurosClientTest
+    {
+        private readonly TaxaDeJurosClient _sut;
+        private readonly Mock<IHttpClientFactory> _taxaDeJurosMock = new Mock<IHttpClientFactory>();
+        private readonly Mock<IOptions<ServiceSettings>> _settingsMock = new Mock<IOptions<ServiceSettings>>();
+
+        public TaxaDeJurosClientTest()
+        {
+            _sut = new TaxaDeJurosClient(_taxaDeJurosMock.Object,_settingsMock.Object);
+        }
+
+        [Fact]
+        public async Task ObterTaxaDeJuros_DeveRetornarOValorDeJuros()
+        {
+            var client = new HttpClient();
+            _taxaDeJurosMock.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(client);
+
+
+            var juros = await _sut.ObterTaxaDeJuros();
+        }
+    }
+}
